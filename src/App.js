50eb9ca98bc1c8adb91eee07d0d447f2ca112c26
Duong Quang <dnquang0301@gmail.com>
2022-10-16 17:37:28 +0700

    Rebuild UI Overview screen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 import Table from 'rc-table';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Overview from './Screens/Overview';
-import Details from './Screens/Details';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Overview from './screens/Overview/Overview';
+import Details from './screens/Detail/Details';
 
 function App() {
-  return(
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path = '/Overview' element = {<Overview/>}/>
-        <Route path = '/Details' element = {<Details/>}/>
+        <Route path="/Overview" element={<Overview />} />
+        <Route path="/Details" element={<Details />} />
       </Routes>
     </BrowserRouter>
   );
-
 }
 
 export default App;
-
-
